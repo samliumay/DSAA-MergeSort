4571c127f9f2a23480f80e7260f36f763c9d64aa
Umay ŞAMLI <76824752+samliumay@users.noreply.github.com>
2024-06-27 09:20:35 +0000

    MergeSort

---
+++ src/main/java/MergeSort.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class MergeSort{
+
+  private int[] merge(int [] array1, int [] array2){
+    int []combined = new int[array1.length + array2.length];
+    int index = 0; 
+    int i = 0;
+    int j = 0;
+
+    while(i < array1.length && j < array2.length){
+      if(array1[i] > array2[j]){
+        combined[index] = array1[i];
+        index++;
+        i++;
+      } else {
+        combined[index] = array2[j];
+        index++;
+        j++;
+      }
+      while(i < array1.length){
+        combined[index] = array1[i];
+        index++;
+        i++;
+      }
+      while(j < array2.length){
+        combined[index] = array2[j];
+        index++;
+        j++;
+      } 
+    }
+     return combined;
+  }
+
+  public int [] mergeSort(int [] array){
+
+    if(array.length == 1){
+      return array;
+    }
+    
+    int midIndex = array.length/2;
+    int [] left = mergeSort(Arrays.copyOfRange(array, 0, midIndex));
+
+    int [] right = mergeSort(Arrays.copyOfRange(array,midIndex,array.length));
+
+    return merge(left, right);
+
+    
+    
+    
+  }
+
+  
+  
+}
